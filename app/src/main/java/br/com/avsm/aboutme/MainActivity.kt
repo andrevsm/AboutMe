ebdfb0a5a3f3ef332a974e475d715bb1f24bf6bc
Andre Melo <andrevsm@Andres-MacBook-Air.local>
2021-06-02 13:33:11 -0300

    Added data binding

--- app/src/main/java/br/com/avsm/aboutme/MainActivity.kt
+++ app/src/main/java/br/com/avsm/aboutme/MainActivity.kt
@@ -5,28 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import br.com.avsm.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
+    private val myName: MyName = MyName("Andre Melo")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        dataBinding.nameText.text = myName.name
         dataBinding.doneButton.setOnClickListener { addNickname(it) }
     }
 
     private fun addNickname(view: View) {
         dataBinding.apply {
-            dataBinding.nicknameText.text = dataBinding.nicknameEdit.text
+            myName?.nickName = nicknameEdit.text.toString()
             invalidateAll()
-            dataBinding.nicknameEdit.visibility = View.GONE
-            dataBinding.doneButton.visibility = View.GONE
-            dataBinding.nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
         }
 
         // Hide the keyboard.
